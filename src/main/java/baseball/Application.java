7b97feaed51c8c04be6f88d0cec7d1e7ca6872e4
curiousKidd <skasrjhaan@gmail.com>
2025-03-19 23:51:32 +0900

    20250319(수) 기능 구현 변경 playGame method 구현 ComputerNumber 객체화 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,17 +11,22 @@ public class Application {
     // 게임 반복 boolean
     static boolean isGameReload = true;
 
-    // 컴퓨터 숫자
-    static Integer[] computerNumber = getComputerNumber();
-
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
         do {
-            // user 숫자
+            // 새로운 게임 시작 (새로운 ComputerNumber 객체 생성)
+            ComputerNumber computerNumber = new ComputerNumber();
+            playGame(computerNumber);
+        } while (isGameReload);
+    }
+
+    // 기존 게임 로직 method 변경
+    private static void playGame(ComputerNumber computerNumber) {
+        while (true) {
+            // 사용자 숫자 입력
             int[] userNumbers = setUserWriteNumber();
 
             // 유저 숫자, 컴퓨터 숫자 비교 및 카운팅
-            GameResult gameResult = getNumberMatchCount(userNumbers);
+            GameResult gameResult = getNumberMatchCount(userNumbers, computerNumber);
 
             // 결과 출력
             System.out.println(gameResult);
@@ -29,9 +34,9 @@ public class Application {
             // 3스트라이크일 경우 재시작 OR 끝낼것인지 체킹
             if (gameResult.isThreeStrikes()) {
                 gameReloadOrEnd();
+                break;
             }
-        } while (isGameReload);
-
+        }
     }
 
     private static void gameReloadOrEnd() {
@@ -41,22 +46,9 @@ public class Application {
 
         if (Integer.parseInt(endNumber) == 1) {
             System.out.println("게임 끝");
-            computerNumber = getComputerNumber();
         } else {
             isGameReload = false;
         }
-    }
-
-    // 컴퓨터 번호
-    private static Integer[] getComputerNumber() {
-        // 중복 방지
-        Set<Integer> set = new LinkedHashSet<>(3);
-
-        while (set.size() != 3) {
-            set.add(Randoms.pickNumberInRange(1, 9));
-        }
-
-        return set.toArray(new Integer[3]);
     }
 
     // 사용자 번호 입력
@@ -65,37 +57,32 @@ public class Application {
         String s = Console.readLine();
 
         String[] split = s.split("");
-
         int[] userNumbers = new int[3];
         for (int i = 0; i < 3; i++) {
             userNumbers[i] = Integer.parseInt(split[i]);
         }
-
         return userNumbers;
     }
 
     // 번호 매칭
-    private static GameResult getNumberMatchCount(int[] userNumbers) {
+    private static GameResult getNumberMatchCount(int[] userNumbers, ComputerNumber computerNumber) {
         int ball = 0, strike = 0;
+        Integer[] numbers = computerNumber.getNumbers();
 
         for (int i = 0; i < 3; i++) {
             int number = userNumbers[i];
-
             for (int j = 0; j < 3; j++) {
-                if (computerNumber[j] == number && i == j) {
+                if (numbers[j].equals(number) && i == j) {
                     strike++;
                     break;
-
-                } else if (computerNumber[j] == number) {
+                } else if (numbers[j].equals(number)) {
                     ball++;
                     break;
                 }
             }
         }
-
         return new GameResult(strike, ball);
     }
-
 }
 
 class GameResult {
@@ -123,7 +110,27 @@ class GameResult {
         if (balls > 0) {
             sb.append(balls).append("볼");
         }
-        return sb.toString();
+        return sb.toString().trim();
     }
 }
 
+// 컴퓨터 번호 부분 객체화
+class ComputerNumber {
+    private final Integer[] numbers;
+
+    public ComputerNumber() {
+        this.numbers = generateNumbers();
+    }
+
+    private Integer[] generateNumbers() {
+        Set<Integer> set = new LinkedHashSet<>(3);
+        while (set.size() < 3) {
+            set.add(Randoms.pickNumberInRange(1, 9));
+        }
+        return set.toArray(new Integer[3]);
+    }
+
+    public Integer[] getNumbers() {
+        return numbers.clone(); // 외부에서 수정되지 않도록 방어적 복사
+    }
+}
