50d3f2740ab79634f9904eeb2dab3787075486cb
curiousKidd <skasrjhaan@gmail.com>
2025-04-07 23:47:25 +0900

    test

--- src/main/java/baseball/DTO/UserNumber.java
+++ src/main/java/baseball/DTO/UserNumber.java
@@ -4,18 +4,18 @@ package baseball.DTO;
 public class UserNumber {
     private Number[] numbers;
 
-    //    public UserNumber(String s) {
-    //        this.numbers = generateNumbers(s);
-    //    }
-    //
-    //    private Number[] generateNumbers(String s) {
-    //        String[] split = s.split("");
-    //        int[] userNumbers = new int[3];
-    //        for (int i = 0; i < 3; i++) {
-    //            numbers[i] = Integer.parseInt(split[i]);
-    //        }
-    //        return numbers;
-    //    }
+    public UserNumber(String s) {
+        this.numbers = generateNumbers(s);
+    }
+
+    private Number[] generateNumbers(String s) {
+        String[] split = s.split("");
+        int[] userNumbers = new int[3];
+        //        for (int i = 0; i < 3; i++) {
+        //            numbers[i] = Integer.parseInt(split[i]);
+        //        }
+        return numbers;
+    }
 
     public Number[] getNumbers() {
         return numbers.clone(); // 외부에서 수정되지 않도록 방어적 복사
